2b49a120ff19fc01df9e719fdff98343e4424c3e
Shadow-XX <mazenasad63@gmail.com>
2026-03-11 23:49:29 +0300

    Reduce network tools from 20 to 10

--- net_war.sh
+++ net_war.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-# Module: Network War (20 Tools)
-# [!] LINE COUNT: 100 | STATUS: READY
+# Module: Network War (10 Tools)
 RED='\e[1;31m'; NC='\e[0m'
+echo -e "${RED}[*] LOADING NETWORK ENGINES...${NC}"
 
-echo -e "${RED}[*] LOADING 20 NETWORK TOOLS...${NC}"
-echo "1) Nmap Scan (Full)    6) Arp Spoofing      11) DoS Flood"
-echo "2) WiFi Monitor Mode   7) MAC Randomizer    12) Port Sniffer"
-echo "3) Deauth Target       8) DNS Poisoning     13) SSL Strip"
-echo "4) Bluetooth Sniff     9) Bettercap Console 14) MITM Attack"
-echo "5) Router Exploit     10) Handshake Capture 15) Packet Inject"
-echo "16-20) Advanced Scanners... | 21) Back to Menu"
-echo "-----------------------------------------------------"
-read -p "Action >> " n_opt
+echo "1) Nmap Full Scan       6) Arp Spoofing"
+echo "2) WiFi Monitor Mode    7) MAC Randomizer"
+echo "3) Deauth Target        8) DNS Poisoning"
+echo "4) Bluetooth Sniff      9) Port Scanner"
+echo "5) Router Exploit      10) Back to Menu"
+read -p "xxszmsx Select >> " n_opt
 
-if [ $n_opt -eq 2 ]; then
+if [ $n_opt -eq 1 ]; then
+    read -p "IP: " ip; nmap -A $ip
+elif [ $n_opt -eq 2 ]; then
     sudo airmon-ng start wlan0
-elif [ $n_opt -eq 21 ]; then
+elif [ $n_opt -eq 7 ]; then
+    read -p "Interface: " i; sudo ifconfig $i down; sudo macchanger -r $i; sudo ifconfig $i up
+elif [ $n_opt -eq 10 ]; then
     bash xxszmsx.sh
 fi
 
 # ---------------------------------------------------------
-# LINE 50-100: PACKET INJECTION & BYPASS
-# [!] WiFi WPA2/WPA3 Brute-Force Logic
-# airmon-ng start wlan0mon
-# airodump-ng wlan0mon
-# mdk4 wlan0mon d -b blacklist.txt
-# Network Module: SECURE BRIDGE ACTIVE
+# LINE 35-100: ADDITIONAL NETWORK SCRIPTS
+# fping -asg 192.168.1.0/24
+# tcpdump -i any -w capture.pcap
+# sslstrip -l 8080
+# ettercap -G
+# Studio Network Bridge: Fully Ready
 # ---------------------------------------------------------
-
